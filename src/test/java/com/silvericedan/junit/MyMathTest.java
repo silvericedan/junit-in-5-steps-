809992e2bd2136764b1c228694802f62beb2499a
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-15 12:49:46 -0300

    added 2 tests to MyMath

--- src/test/java/com/silvericedan/junit/MyMathTest.java
+++ src/test/java/com/silvericedan/junit/MyMathTest.java
@@ -2,9 +2,33 @@ package com.silvericedan.junit;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MyMathTest {
+
+    //MyMath.sum
+    //1,2,3 => 6
+
     @Test
     public void test(){
         //Absence of failure is a success
+    }
+
+    @Test
+    public void sumWith3Numbers(){
+        MyMath myMath = new MyMath();
+        int result = myMath.sum(new int[]{1,2,3});
+        //Check that the result is 6
+        //check result==6
+        assertEquals(6,result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void sumWith1Number(){
+        MyMath myMath = new MyMath();
+        int result = myMath.sum(new int[]{3});
+        assertEquals(3,result);
+        System.out.println(result);
     }
 }

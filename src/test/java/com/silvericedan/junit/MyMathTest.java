ab617931aee217fd9a984ffb352983565db821a7
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-15 12:41:40 -0300

    added first test

---
+++ src/test/java/com/silvericedan/junit/MyMathTest.java
@@ -0,0 +1,10 @@
+package com.silvericedan.junit;
+
+import org.junit.jupiter.api.Test;
+
+public class MyMathTest {
+    @Test
+    public void test(){
+        //Absence of failure is a success
+    }
+}
